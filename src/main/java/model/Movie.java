16c68223b6356e235d96db9e66f5cf46c4270a01
Adityaxgit-hub <chintapallia.ug24.cs@nitp.ac.in>
2025-10-21 13:04:39 +0530

    Movie.java

--- src/main/java/model/Movie.java
+++ src/main/java/model/Movie.java
@@ -1,3 +1,40 @@
+
+package model;
+
+import java.util.*;
+
 public class Movie {
-    
+    private String title;
+    private List<String> genres;
+    private double rating;
+    private String overview;
+
+    public Movie(String title, List<String> genres, double rating, String overview) {
+
+        this.title = title;
+        this.genres = genres;
+        this.rating = rating;
+        this.overview = overview;
+    }
+
+    public String getDetails() {
+        return "Title:" + title + "\n Genres:" + genres + "\nRating:" + rating + "Stars";
+
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public List<String> getgenres() {
+        return genres;
+    }
+
+    public double getRating() {
+        return rating;
+    }
+
+    public String getOverview() {
+        return overview;
+    }
 }
