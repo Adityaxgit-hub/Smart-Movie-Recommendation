b7bd9dabeaf215cdf1e450104a9f531b05003899
Adityaxgit-hub <chintapallia.ug24.cs@nitp.ac.in>
2025-10-24 11:58:50 +0530

    Recommender done

--- src/main/java/service/Recommender.java
+++ src/main/java/service/Recommender.java
@@ -1,5 +1,21 @@
+package service;
 
+import model.Movie;
+import model.User;
+import java.util.*;
 
 public class Recommender {
-    
+    public List<Movie> recommendMovies(User user , List<Movie>allMovies){
+        List<Movie>recommend=new ArrayList<>();
+
+        for(Movie movie : allMovies){
+            for(String prefGenre :user.getpref()){
+                if(movie.getgenres().contains(prefGenre)){
+                    recommend.add(movie);
+                    break;
+                }
+            }
+        }
+        return recommend;
+    }
 }
